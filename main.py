c43e2f21cda3078a2c5fd7dfd4d8cb03e65d1579
Катерина <katyfar2008@mail.ru>
2024-02-24 20:33:03 +0300

    comm

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/training/<prof>')
 def index(prof):
-    return render_template('base.html',  prof=prof)
+    img1 = url_for('static', filename='img/spaceship_1.jpg')
+    img2 = url_for('static', filename='img/spaceship_2.png')
+    css = url_for('static', filename='css/style.css')
+    return render_template('base.html',  prof=prof, img1=img1, img2=img2, css=css)
 
 
 if __name__ == '__main__':
